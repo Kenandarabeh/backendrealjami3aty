18898a026b3f915bd932e938af111816610b5ee2
kenanda abdelouahad rabah <104671864+Kenandarabeh@users.noreply.github.com>
2023-08-03 07:12:41 +1300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://jami3atyf.onrender.com',
     credentials: true
 }));
 
